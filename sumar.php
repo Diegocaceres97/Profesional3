df092af47fdeb8253d65230b666edc82958c763a
Diego <diegocaceres1997garcia@gmail.com>
2020-09-19 07:11:17 -0500

    first commit

---
+++ sumar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sumar.</title>
+</head>
+<body>
+    <?php
+    $valor1 = $_POST["T1"];
+    $valor2 = $_POST["T2"];
+    $valor3 = $_POST["T3"];
+    $suma = $valor1 + $valor2 + $valor3;
+    echo "$valor1 + $valor2 + $valor3 = $suma"; ?>
+</body>
+</html>
